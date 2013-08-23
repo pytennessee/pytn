da33b426b0fd98cb5965f0e9464467b9350ad5dd
Jason Myers <jason@jasonamyers.com>
2013-08-23 10:30:56 -0500

    Playing with out dj_assets
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- pytn/wsgi.py
+++ pytn/wsgi.py
@@ -3,6 +3,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pytn.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+application = get_wsgi_application()

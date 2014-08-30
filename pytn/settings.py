c26910d99d2af732aaa7a605f4784c88788a933c
Jason Myers <jason@jasonamyers.com>
2014-08-29 21:15:57 -0500

    Prepend www
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- pytn/settings.py
+++ pytn/settings.py
@@ -39,6 +39,8 @@ DATABASES = {
 # Parse database configuration from $DATABASE_URL
 # import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
+PREPEND_WWW = True
 #
 # # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

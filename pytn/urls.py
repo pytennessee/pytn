42fd32b34c2a574b9b44da4d134393404f3d4201
Jason Myers <jason@jasonamyers.com>
2016-07-26 08:01:46 -0500

    Ahh letsencrypt
    Signed-off-by: Jason Myers <jason@jasonamyers.com>

--- pytn/urls.py
+++ pytn/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns("",
     url(r"^$", direct_to_template, {
         "template": "homepage.html",
     }, name="home"),
-    url("^.well-known/acme-challenge/g7PXzzy82-9oreI3kQtbdsgSvQz4lBFS88nGvZAd8Iw", stickers.views.lets_encrypt, name="lets_encrypt"),
+    url("^.well-known/acme-challenge/cXmiSv4yzjJNmENOf1r6RSv1yzpdrEaZ1sN4SfNNRF0", stickers.views.lets_encrypt, name="lets_encrypt"),
     url(r"^admin/", include(admin.site.urls)),
 
     url(r"^account/signup/$", symposion.views.SignupView.as_view(), name="account_signup"),
